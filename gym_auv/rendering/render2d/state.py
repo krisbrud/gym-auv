3cce2f44a2813592ed3c9779a2310defdb4cccfa
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-09-26 10:22:07 +0200

    Made RenderableState to make interface for rendering

---
+++ gym_auv/rendering/render2d/state.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import List
+
+from numpy import ndarray
+from gym_auv.objects.vessel import Vessel
+from gym_auv.objects.obstacles import BaseObstacle
+
+
+@dataclass
+class RenderableState:
+    """RenderableState contains all the information the renderer needs to render the environment"""
+
+    # Text
+    last_reward: float
+    cumulative_reward: float
+    t_step: float
+    episode: int
+    lambda_tradeoff: float
+    eta: float
+
+    # Vessel
+    vessel: Vessel
+    obstacles: List[BaseObstacle]
+
+    # Path
+    path_points: ndarray
+    path_taken: ndarray
