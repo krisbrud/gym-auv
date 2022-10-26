8b6fd887bb58a6d2e62542bd33d34a5eddd85db2
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-10-26 10:57:43 +0000

    Added second test to hierarchical collision detector

--- tests/test_hierarchical_collision_detector.py
+++ tests/test_hierarchical_collision_detector.py
@@ -14,10 +14,12 @@ def my_vessel() -> Vessel:
     return vessel
 
 
-@pytest.fixture
-def obst_behind_vessel() -> CircularObstacle:
-    pos = np.array([0, -9.5])
-    # pos = np.array([0, 0])
+@pytest.fixture(params=[True, False])
+def obst_behind_vessel(request) -> CircularObstacle:
+    if request.param:
+        pos = np.array([0, -9.5])
+    else:
+        pos = np.array([0, -10.5])
     radius = 1.5 
 
     obst = CircularObstacle(pos, radius)
@@ -30,19 +32,22 @@ def ranges(my_vessel: Vessel, obst_behind_vessel: CircularObstacle) -> np.ndarra
     ranges, velocities = my_vessel.perceive(obstacles=obstacles)
     return ranges
 
+@pytest.fixture
+def max_range(my_vessel) -> float:
+    return my_vessel.config.sensor.range
 
-def _is_intercepted(range: float):
-    return 0 < range < 1
+def _is_intercepted(range: float, max_range: float):
+    return 0 < range < max_range
 
 
-def test_no_obst_in_front(ranges: np.ndarray):
+def test_no_obst_in_front(ranges: np.ndarray, max_range: float):
     idx_front = len(ranges) // 2
-    assert not _is_intercepted(ranges[idx_front])
+    assert not _is_intercepted(ranges[idx_front], max_range)
 
 
-def test_obst_in_last_sensor(ranges: np.ndarray):
-    assert _is_intercepted(ranges[-1])
+def test_obst_in_last_sensor(ranges: np.ndarray, max_range: float):
+    assert _is_intercepted(ranges[-1], max_range)
 
 
-def test_obst_in_first_sensor(ranges: np.ndarray):
-    assert _is_intercepted(ranges[0])
+def test_obst_in_first_sensor(ranges: np.ndarray, max_range: float):
+    assert _is_intercepted(ranges[0], max_range)
