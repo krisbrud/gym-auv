6394d94e8abcbf8af1338daed2a98603d4c8f45e
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-10-28 14:34:04 +0000

    Changed range for rudder to -1 to 1

--- gym_auv/objects/vessel/vessel.py
+++ gym_auv/objects/vessel/vessel.py
@@ -483,7 +483,7 @@ class Vessel:
         return state_dot
 
     def _thrust_surge(self, surge):
-        surge = np.clip(surge, 0, 1)
+        surge = np.clip((surge + 1.0) / 2, 0, 1)
         return surge * self.config.vessel.thrust_max_auv
 
     def _moment_steer(self, steer):
