328d11f71159f3378e4efe90456a7e7360da44f3
Kristian Brudeli <krbrudeli@gmail.com>
2023-01-15 18:19:24 +0100

    WIP on cleaning up script

--- scripts/play_gym_auv.py
+++ scripts/play_gym_auv.py
@@ -1,27 +1,42 @@
 # %% 
+import os
 import gym
 import gym_auv
 import copy
+import pickle
 
-env_name = "MovingObstaclesLosRewarder-v0" 
-
-gym_auv_config = copy.deepcopy(gym_auv.MOVING_CONFIG)
-gym_auv_config.episode.use_terminated_truncated_step_api = True
-
-env = gym.make(env_name, env_config=gym_auv_config)
-
-# obs = env.reset()
-
-# Make lunar lander env with human rendering
-# env = gym.make("LunarLander-v2", render_mode="rgb_array") # , render_mode="human")
-# env = gym.make("LunarLander-v2", render_mode="human")
-
-# # Make it a playable game
 from gym.utils.play import play, PlayPlot
 import numpy as np
 
-# def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
-#     return [rew,]
+
+gym_auv_config = copy.deepcopy(gym_auv.MOVING_CONFIG)
+gym_auv_config.episode.use_terminated_truncated_step_api = True
+gym_auv_config.episode.return_latest_data_in_info = True
+
+env_name = "MovingObstaclesLosRewarder-v0" 
+env = gym.make(env_name, env_config=gym_auv_config)
+
+
+rewards = []
+latest_data = []
+done_indices = []
+step_idx = 0
+
+def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
+    global step_idx
+    global rewards
+    global done_indices
+    global latest_data
+
+    done = truncated or terminated
+    if done:
+        done_indices.append(step_idx)
+    step_idx += 1  # Note: This is the step index of the _next_ step
+
+    rewards.append(rew)
+    latest_data.append(copy.deepcopy(info["latest_data"]))
+
+    return [rew,]
 # plotter = PlayPlot(callback, 150, ["reward"])
 
 keys_to_action = {
@@ -35,8 +50,37 @@ keys_to_action = {
 
 noop = np.array([0, 0])
 
-play(env, keys_to_action=keys_to_action, fps=10, noop=noop), 
+play(env, keys_to_action=keys_to_action, fps=10, noop=noop, callback=callback)
 # callback=plotter.callback)
 
+# print(rewards)
+# print("len rewards",len(rewards))
+# # print(latest_data)
+# print("len latest", len(latest_data))
+# print(f"{step_idx = }")
 
+def get_file_path(file_name):
+    if not file_name.endswith(".pkl"):
+        file_name += ".pkl"
+
+    subdir_name = "play_episodes"  # Subdirectory of the directory this script is in
+
+    # construct the path to the subdirectory
+    subdir_path = os.path.join(os.path.dirname(__file__), subdir_name)
+
+    # construct the path to the file in the subdirectory
+    file_path = os.path.join(subdir_path, file_name)
+
+    return file_path
+
+file_name = input("Enter file name for saving episode data (press enter to not save): ").strip()
+if file_name: # If not empty or only whitespace (removed by strip)
+    print("Only saving first episode. Run the script again to save more episodes.")
+
+    file_path = get_file_path(file_name)
+
+    episode_latest_data = latest_data[:done_indices[0]]
+
+    with open(file_name, "wb") as f:
+        pickle.dump(episode_latest_data, f)
 # %%
